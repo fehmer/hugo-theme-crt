101c95d939aaf39ad4ef9b2b282c07e9c25c9517
Christian Fehmer <fehmer@users.noreply.github.com>
2026-07-28 10:33:00 +0200

    update hugo to v0.164.0 (#4)

---
+++ dev.sh
@@ -0,0 +1,8 @@
+docker run --rm \
+  --name mysite \
+  -p 8080:8080 \
+  -v ${PWD}/exampleSite:/src \
+  -v ${PWD}:/src/themes/hugo-theme-crt \
+  -v ${HOME}/hugo_cache:/tmp/hugo_cache \
+  hugomods/hugo:debian-non-root-0.164.0  \
+  server -p 8080 --disableFastRender
